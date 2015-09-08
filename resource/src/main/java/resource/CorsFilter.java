927128efbdaca59ffccf658134666743b25499b8
jorgeisxchange <jorgei@sxchange.com.ph>
2015-09-08 14:35:53 +0800

    soft remove CorsFilter

--- resource/src/main/java/resource/CorsFilter.java
+++ resource/src/main/java/resource/CorsFilter.java
@@ -1,25 +1,8 @@
 package resource;
 
-import java.io.IOException;
+public class CorsFilter {
 
-import javax.servlet.Filter;
-import javax.servlet.FilterChain;
-import javax.servlet.FilterConfig;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import org.springframework.core.Ordered;
-import org.springframework.core.annotation.Order;
-import org.springframework.stereotype.Component;
-
-@Component
-@Order(Ordered.HIGHEST_PRECEDENCE)
-public class CorsFilter implements Filter {
-
-	@Override
+/*	@Override
 	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
 			throws IOException, ServletException {
 		
@@ -44,7 +27,7 @@ public class CorsFilter implements Filter {
 	@Override
 	public void init(FilterConfig arg0) throws ServletException {
 		
-	}
+	}*/
 	
 	
 }
